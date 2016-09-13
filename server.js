020ec48194309bb83237174d503943c11fd41fb8
Patricio <patricio.chavez@telefonica.com>
2016-09-13 17:48:22 -0300

    desde mi pc

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hola Mundo! desde un commit en github");
+  response.end("Hola Mundo! desde un commit en github sobre OSE");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
