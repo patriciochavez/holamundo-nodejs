51e6953f317c8c777d891b17e93cc9fbb3ca05cb
Patricio <patricio.chavez@telefonica.com>
2016-09-13 17:51:55 -0300

    sin SSL

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hola Mundo! desde un commit en github sobre OSE v3");
+  response.end("Hola Mundo! desde un commit en github sobre OSE v3 sin SSL");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
