cdf7d0a887799f8ccdc406d38cd5325a772f3e89
patriciochavez <patricio_chavez@hotmail.com>
2016-09-13 17:46:44 -0300

    commit1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("CONTAINER: Hola Mundo!");
+  response.end("Hola Mundo! desde un commit en github");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
