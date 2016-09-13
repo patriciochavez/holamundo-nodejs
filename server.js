c22e23c3ef3482d1885db1f709698cf2cda4e0eb
patriciochavez <patricio_chavez@hotmail.com>
2016-09-13 17:49:26 -0300

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.end("Hola Mundo! desde un commit en github sobre OSE");
+  response.end("Hola Mundo! desde un commit en github sobre OSE v3");
 }
 var www = http.createServer(handleRequest);
 www.listen(8080);
