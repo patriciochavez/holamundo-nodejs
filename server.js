4ca4e09935375e2df69e5339972d82b1f8542a75
Patricio <patricio.chavez@telefonica.com>
2016-09-14 11:02:59 -0300

    cambio de comillas

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var handleRequest = function(request, response) {
   response.writeHead(200);
-  response.write("<img style='-webkit-user-select: none; cursor: zoom-out;' src='http://www.empresas.telefonica.com.ar/wp-content/themes/telefonica_empresas/img/bg/bg_productos.jpg' width='1366' height='784'>");
+  response.write('<img style="-webkit-user-select: none; cursor: zoom-out;" src="http://www.empresas.telefonica.com.ar/wp-content/themes/telefonica_empresas/img/bg/bg_productos.jpg" width="1366" height="784">');
   response.end();
 }
 var www = http.createServer(handleRequest);
